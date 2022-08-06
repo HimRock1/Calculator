21c5a44d5224ef603af6367af2eaf005b2799ec6
Himjyoti <himjyotisarma@tutanota.com>
2022-08-06 14:33:12 +0530

    Add Operate Function when any action is clicked

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ const Division=function(first,...args){
     const result=args.reduce((total,arg)=>total/arg,first);
     return result;
 }
+
+
+// function OPERATE that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(operator,num1,num2){
+    let operation;
+    if(operator=="+"){
+        operation=addition(num1,num2);
+    }
+    else if(operator=="-"){
+        operation=subtraction(num1,num2);
+    }
+    else if(operator=="*"){
+        operation=multiply(num1,num2);
+    }
+    else if(operator=="/"){
+        operation=Division(num1,num2);
+    }
+    return operation;
+}
